c6bb5601619dc1c8ae6b6a00495e835a262634e6
baihuayang <1053091115@qq.com>
2019-03-10 14:37:54 +0800

    Spring Cloud学习demo

--- microservice-provider/src/main/java/com/baihy/MicroserviceProviderApplication.java
+++ microservice-provider/src/main/java/com/baihy/MicroserviceProviderApplication.java
@@ -2,6 +2,7 @@ package com.baihy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @ProjectName: microservice-cloud
@@ -11,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: 2019-03-09 17:22
  */
 @SpringBootApplication
+@EnableEurekaClient
+// 标识这个服务是需要注册到eureka server的列表中
 public class MicroserviceProviderApplication {
 
     public static void main(String[] args) {
